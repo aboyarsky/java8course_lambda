2033f4e8b17593bd4dbcaf9144f864037b10029c
aboyarsky <iamnotbotan@mail.ru>
2018-01-18 20:48:45 +0300

    added test4

--- src/test/java/lambda/part2/exercise/TestArrowNotationExercise.java
+++ src/test/java/lambda/part2/exercise/TestArrowNotationExercise.java
@@ -59,5 +59,10 @@ public class TestArrowNotationExercise {
                         new Person("aa", "b", 1)));
     }
 
+    @Test
+    public void alwaysPasses(){
+        assertEquals(2 * 2, 4);
+    }
+
 
 }
